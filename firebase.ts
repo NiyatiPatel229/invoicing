24d8284105165a32352d92572457f05fbc8f4abc
NiyatiPatel229 <22aiml037@charust.edu.in>
2025-12-28 15:29:55 +0530

    fix firebase keys

--- firebase.ts
+++ firebase.ts
@@ -1,13 +1,21 @@
-
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
-const firebaseConfig = { apiKey: "AIzaSyBd3d_1T95NEa3wIF6ih8PicZ1B21koKeA", authDomain: "invoicing-ddd3c.firebaseapp.com", projectId: "invoicing-ddd3c", storageBucket: "invoicing-ddd3c.firebasestorage.app", messagingSenderId: "414608593822", appId: "1:414608593822:web:b57d9e816864f636dc7c8f", measurementId: "G-0TMKVJ1WHX" };
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
 
 
-// Initialize Firebase once
+
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
